f907f9dcdd68018c26752f67f3e8b479edaf0f11
arderh1x <whereyoursoul@gmail.com>
2026-03-31 14:01:40 +0300

    added title to Pages Router (lw10)

--- labwork10-PR/src/pages/index.tsx
+++ labwork10-PR/src/pages/index.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import styles from "../styles/index.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Labwork 10 - Pages Router</title>
+      </Head>
+
       <h1>Default main page. Nothing here.</h1>
       <h6 className={styles.moduleH6}>colored text</h6>
     </div>
